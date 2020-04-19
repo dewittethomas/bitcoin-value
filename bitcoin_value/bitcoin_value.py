6c0ec4ca4ccd802bbea29c4918c10ba0cb445629
Thomas Dewitte <28838900+dewittethomas@users.noreply.github.com>
2020-04-19 03:15:05 +0200

    Removed round

--- bitcoin_value/bitcoin_value.py
+++ bitcoin_value/bitcoin_value.py
@@ -5,10 +5,10 @@ res = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
 json_data = json.loads(res.text)
 
 def EUR():
-    return "€" + str(round(json_data['bpi']['EUR']['rate_float'], 2))
+    return "€" + str(json_data['bpi']['EUR']['rate_float'])
 
 def USD():
-    return "$" + str(round(json_data['bpi']['USD']['rate_float'], 2))
+    return "$" + str(json_data['bpi']['USD']['rate_float'])
 
 def GBP():
-    return "£" + str(round(json_data['bpi']['GBP']['rate_float'], 2))
+    return "£" + str(json_data['bpi']['GBP']['rate_float'])
