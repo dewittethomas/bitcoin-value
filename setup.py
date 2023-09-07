3cece398d17518a9dc0479c689e035acb05b7051
Thomas Dewitte <thomasdewittecontact@gmail.com>
2023-09-07 22:22:31 +0200

    Updated content

--- setup.py
+++ setup.py
@@ -5,27 +5,26 @@ with open("README.md", "r") as fh:
 
 setup(
     name = "bitcoin_value",
-    version = "1.5.2",
+    version = "1.6.0",
     author = "Thomas Dewitte",
     author_email = "thomasdewittecontact@gmail.com",
     license = "MIT",
     url = "https://github.com/dewittethomas/bitcoin-value",
     
-    description = "A tracker that gets the latest value of Bitcoin in any currency",
+    description = "A module to fetch the latest value of Bitcoin in different currencies.",
     long_description = long_description,
     long_description_content_type = "text/markdown",
 
     package_dir = {"bitcoin_value": "bitcoin_value"},
     install_requires = [
-        "requests>=2.22.0"
+        "requests>=2.28.1"
     ],
 
     packages = find_packages(),
 
     classifiers = [
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
     ],
